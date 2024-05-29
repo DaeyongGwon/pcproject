ea817b275759983b38d9e55e5ea45fd3e6b038f0
dae <cys964@gmail.com>
2024-05-29 11:19:30 +0900

    프로젝트 기초 세팅

---
+++ src/main/java/com/pcroom/pcproject/view/MainPage.java
@@ -0,0 +1,23 @@
+package com.pcroom.pcproject.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainPage extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainPage.class.getResource("MainPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.pcroom.pcproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.pcroom.pcproject.view to javafx.fxml;
+    exports com.pcroom.pcproject.view;
+}
