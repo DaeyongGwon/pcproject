986b59099a41970dd3bb5a113202af6faa7b2c62
kjs0314 <168392209+kjs0314@users.noreply.github.com>
2024-05-29 11:53:07 +0900

    Merge branch 'main' into sub

--- src/main/java/com/pcroom/pcproject/view/MainPage.java
+++ src/main/java/com/pcroom/pcproject/view/MainPage.java
@@ -12,7 +12,7 @@ public class MainPage extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainPage.class.getResource("MainPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!123555555555");
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
