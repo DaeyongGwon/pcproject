74fb52795f89c7f57fdedb83bd2e4b30a66a4fd9
dae <cys964@gmail.com>
2024-05-29 11:31:33 +0900

    깃 테스트
    테스트 123123

--- src/main/java/com/pcroom/pcproject/view/MainPage.java
+++ src/main/java/com/pcroom/pcproject/view/MainPage.java
@@ -12,7 +12,7 @@ public class MainPage extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainPage.class.getResource("MainPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello!123");
         stage.setScene(scene);
         stage.show();
     }
