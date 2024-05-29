6e3e83dfc5e3d0445fc0fa6c4e6b5be19a7e340f
dae <cys964@gmail.com>
2024-05-29 20:57:42 +0900

    controller 사용을 위해서모듈 설정

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,10 @@ module com.pcroom.pcproject {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens com.pcroom.pcproject.controller to javafx.fxml;
     opens com.pcroom.pcproject.view to javafx.fxml;
+
     exports com.pcroom.pcproject.view;
+    exports com.pcroom.pcproject.controller;
+
 }
