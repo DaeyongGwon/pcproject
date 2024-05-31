aa34004e8ebc9a6f481b463222ef82a5668b6e79
dae <cys964@gmail.com>
2024-05-31 16:20:52 +0900

    module-info sql 연결

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.pcroom.pcproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.pcroom.pcproject.controller to javafx.fxml;
     opens com.pcroom.pcproject.view to javafx.fxml;
