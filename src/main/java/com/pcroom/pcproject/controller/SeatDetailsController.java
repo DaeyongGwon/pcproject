30185b158e06319a326fe4e71c1408458dc13ac5
kjs0314 <168392209+kjs0314@users.noreply.github.com>
2024-06-04 16:11:00 +0900

    SeatDetailsController 수정

--- src/main/java/com/pcroom/pcproject/controller/SeatDetailsController.java
+++ src/main/java/com/pcroom/pcproject/controller/SeatDetailsController.java
@@ -10,6 +10,8 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SeatDetailsController {
     @FXML
@@ -19,23 +21,32 @@ public class SeatDetailsController {
     private Label statusLabel;
 
     @FXML
+    private Label startTimeLabel; // 시작 시간 레이블 추가
+
+    @FXML
+    private Label usernameLabel; // 사용자 이름 레이블 추가
+
+    @FXML
     private Button moveToNewWindowButton;
 
     @FXML
     public void initialize() {
         // 새 창으로 이동하는 버튼의 초기 가시성 설정
-        moveToNewWindowButton.setVisible(!"사용 중".equals(statusLabel.getText()));
+        moveToNewWindowButton.setVisible(!"사용 중인 좌석 입니다.".equals(statusLabel.getText()));
     }
 
     @FXML
     private void moveToNewWindow(ActionEvent event) {
-        if (!"사용 중".equals(statusLabel.getText())) {
+        if (!"사용 중인 좌석 입니다.".equals(statusLabel.getText())) {
             try {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/pcroom/pcproject/view/UserPage.fxml"));
                 Parent root = loader.load();
 
+                UserPageController controller = loader.getController();
+                controller.setUserPageDetails(seatLabel.getText(), usernameLabel.getText(), startTimeLabel.getText());
+
                 Stage stage = new Stage();
-                stage.setTitle("새 창");
+                stage.setTitle("컴퓨터 사용 중");
                 stage.setScene(new Scene(root));
                 stage.show();
 
@@ -48,12 +59,22 @@ public class SeatDetailsController {
         }
     }
 
-    public void setSeatDetails(String seatNumber, String status) {
+    public void setSeatDetails(String seatNumber, String status, String startTime, String username) {
         seatLabel.setText("좌석 번호: " + seatNumber);
         statusLabel.setText("상태: " + status);
+        startTimeLabel.setText(formatTime(startTime)); // 시작 시간 설정
+        usernameLabel.setText(username); // 사용자 이름 설정
 
         // 좌석 상태가 변경될 때마다 새 창으로 이동하는 버튼의 가시성 업데이트
-        moveToNewWindowButton.setVisible(!"사용 중".equals(status));
+        moveToNewWindowButton.setVisible(!"사용 중인 좌석 입니다.".equals(status));
+    }
+
+
+    private String formatTime(String startTime) {
+        // LocalDateTime.parse를 사용하여 문자열을 LocalDateTime 객체로 변환
+        LocalDateTime dateTime = LocalDateTime.parse(startTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        // DateTimeFormatter를 사용하여 시간 부분만 형식화
+        return dateTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
     }
 
     @FXML
